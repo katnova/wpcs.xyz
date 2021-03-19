bc371cd41c1a215d8b393850e7780e8a21f199db
Abstract-Programming <admin@abstracprogramming.org>
2021-03-19 13:29:57 -0600

    Added info/about commands, fixed JS hotspot.
    Signed-off-by: Abstract-Programming <admin@abstracprogramming.org>

--- js/plugins.js
+++ js/plugins.js
@@ -1,15 +1,17 @@
 // Avoid `console` errors in browsers that lack a console.
-(function() {
-  var method;
-  var noop = function () {};
-  var methods = [
+const repo_link = "https://github.com/Abstract-Programming/wpcs.xyz";
+(function () {
+  let method;
+  let noop = function () {
+  };
+  let methods = [
     'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
     'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
     'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
     'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
   ];
-  var length = methods.length;
-  var console = (window.console = window.console || {});
+  let length = methods.length;
+  let console = (window.console = window.console || {});
 
   while (length--) {
     method = methods[length];
@@ -19,27 +21,25 @@
       console[method] = noop;
     }
   }
-  console.log("Hello internet!");
+  console.log('           (         (        )     )    )  \n' +
+    ' (  (      )\\ )  (   )\\ )  ( /(  ( /( ( /(  \n' +
+    ' )\\))(   \'(()/(  )\\ (()/(  )\\()) )\\()))\\()) \n' +
+    '((_)()\\ )  /(_)|((_) /(_))((_)\\ ((_)\\((_)\\  \n' +
+    '_(())\\_)()(_)) )\\___(_))  __((_)_ ((_)_((_) \n' +
+    '\\ \\((_)/ /| _ ((/ __/ __| \\ \\/ | \\ / /_  /  \n' +
+    ' \\ \\/\\/ / |  _/| (__\\__ \\_ >  < \\ V / / /   \n' +
+    '  \\_/\\_/  |_|   \\___|___(_)_/\\_\\ |_| /___|  \n' +
+    '                                            \n' +
+    'Someones poking around in the code, maybe go check out the GitHub repo?\n' +
+    repo_link);
 }());
 
 // Place any jQuery/helper plugins in here.
-jQuery(function($, undefined) {
+jQuery(function ($) {
   $('body').terminal({
     //local compiler commands
-    js: function(command) {
-      console.log("Parse JS: " + command);
-      if (command !== '') {
-        try {
-          var result = window.eval(command);
-          if (result !== undefined) {
-            this.echo(new String(result));
-          }
-        } catch (e) {
-          this.error(new String(e));
-        }
-      } else {
-        this.echo('');
-      }
+    js: function (command) {
+      parseJS(this, command);
     },
     //Help commands
     man: function () {
@@ -48,10 +48,21 @@ jQuery(function($, undefined) {
     help: function () {
       man(this);
     },
+    //image commands
     image: function (url) {
       return fetch_image(url);
+    },
+    github: function () {
+      github(this);
+    },
+    info: function () {
+      info(this);
+    },
+    about: function () {
+      info(this);
     }
-   }, {
+  }, {
+    //Config
     greetings: '           (         (        )     )    )  \n' +
       ' (  (      )\\ )  (   )\\ )  ( /(  ( /( ( /(  \n' +
       ' )\\))(   \'(()/(  )\\ (()/(  )\\()) )\\()))\\()) \n' +
@@ -63,6 +74,8 @@ jQuery(function($, undefined) {
       '                                            \n' +
       '\n',
     name: 'main-term',
+    autocompleteMenu: true,
+    completion: ['man', 'help', 'github', 'js', 'image', 'info', 'about'],
     prompt: 'local@wpcs.xyz $ '
   });
 });
@@ -72,17 +85,57 @@ jQuery(function($, undefined) {
  * reusable functions
  */
 
-function man(context){
+function man(context) {
   context.echo("\nCommands: " +
     "\n 'js <args>'   : You can attempt to run JS commands, this will parse content after \"js \" using window.eval();." +
-    "\n 'image <url>' : Fetch a image from a URL and display it in the console."  +
+    "\n 'image <url>' : Fetch a image from a URL and display it in the console." +
+    "\n 'about'       : Get information about wcps.xyz" +
+    "\n 'github'      : Open the GitHub repo for wcps.xyz " +
     "\n");
 }
 
+function info(context){
+  context.echo("\nAbout wpcs.xyz" +
+    "\nWho knows, its to early to tell what this will turn into." +
+    "\n");
+}
+
+/**
+ * Fetch a image from a remote URL async
+ * @param url Url to fetch
+ * @returns {Promise<unknown>}
+ */
+
 function fetch_image(url) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     const img = $('<img src="' + url + '"/>');
     img.on('load', () => resolve(img));
     img.on('error', reject);
   });
+}
+
+
+/**
+ * functions
+ */
+
+function parseJS(context, command) {
+  console.log("Parse JS: " + command);
+  if (command !== '') {
+    try {
+      var result = window.eval(command);
+      if (result !== undefined) {
+        context.echo(new String(result));
+      }
+    } catch (e) {
+      context.error(new String(e));
+    }
+  } else {
+    context.echo('');
+  }
+}
+
+function github(context) {
+  context.echo("Opening wpcs.xyz repo...");
+  window.open(repo_link);
 }
