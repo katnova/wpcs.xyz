cc9da917f7d362a3c48b39a8f6bde6fe45052b9c
codefactor-io <support@codefactor.io>
2021-03-20 07:55:22 +0000

    [CodeFactor] Apply fixes

--- js/pkg_modules/spinner.js
+++ js/pkg_modules/spinner.js
@@ -1423,7 +1423,7 @@ function start(term, spinner) {
   function set() {
     let text = spinner.frames[i++ % spinner.frames.length];
     term.set_prompt("Working " + text);
-  };
+  }
   prompt = term.get_prompt();
   term.find('.cursor').hide();
   set();
