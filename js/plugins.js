e420f1859abb83675d67d7900ae65209f496dc27
Abstract-Programming <admin@abstracprogramming.org>
2021-03-20 22:10:15 -0600

    Updated man and UX
    Signed-off-by: Abstract-Programming <admin@abstracprogramming.org>

--- js/plugins.js
+++ js/plugins.js
@@ -101,9 +101,9 @@ jQuery(function ($) {
 
 function man(context) {
   context.echo("\nCommands: " +
-    "\n 'js <args>' : You can attempt to run JS commands, this will parse content after \"js \" using window.eval();." +
     "\n 'image <url>' : Fetch a image from a URL and display it in the console." +
     "\n 'load <module>' : Download an run a registered module in browser from the modules server." +
+    "\n 'verify scripts' : Verify that currently loaded scripts are known." +
     "\n '[enable/disable] [modules | module_loading_messages | soft_reload | debug]' : enable or disable options in the config file. " +
     "\n 'info' : Get session information" +
     "\n 'reload' : Reload the page (depends on soft_reload for type)" +
