e64e16b43f27c4e039b878196c3ebf533626f772
William Penrod <admin@abstractprogramming.org>
2021-04-08 23:13:42 -0600

    Small bug fix (accidental extra regex char '\')

--- js/plugins.js
+++ js/plugins.js
@@ -474,7 +474,7 @@ function list_modules(context) {
 
 function isValidURL(string) {
   const res = string.match(
-    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
   );
   return res !== null;
 }
